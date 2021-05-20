4cc6768ae1fc1cca7c82374580988045ceab486b
Atiti1 <atitidestiny17@gmail.com>
2021-05-20 05:50:30 +0100

    new changes

--- server.js
+++ server.js
@@ -16,13 +16,8 @@ const db = knex({
   client: 'pg',
   connection: {
   	connectionString: process.env.DATABASE_URL,
-  	ssl: {
-  		rejectUnauthorized: false
-  	}
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'test',
-    database : 'smartbrain'
+  	ssl: true,
+    password : 'test'
   }
 });
 
