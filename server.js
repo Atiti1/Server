d28f935b8ef3e12bf0d29afe696522e5be08f847
Atiti1 <atitidestiny17@gmail.com>
2021-05-20 22:25:27 +0100

    new changes

--- server.js
+++ server.js
@@ -15,9 +15,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-  	host : '127.0.0.1',
-  	user : 'atiti',
-  	database : 'smartbrain'
+  	connectionString : process.env.DATABASE_URL,
+  	ssl: true
   }
 });
 
